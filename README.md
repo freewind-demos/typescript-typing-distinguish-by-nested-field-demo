80dad2a8de3add8656b63eb7dea6d34fe65d94b9
Freewind <nowindlee@gmail.com>
2024-02-25 20:52:34 +0800

    typescript-typing-distinguish-by-nested-field-demo

--- README.md
+++ README.md
@@ -1,18 +1,9 @@
-TypeScript Hello World Demo
+TypeScript Typing Distinguish by Nested Field Demo
 ===========================
+
+好像不能通过一个内部的字段类型来判断外部对象的类型
 
 ```
 npm install
 npm start
 ```
-
-Note:
-
-`esModuleInterop` is recommended to set to `true`,
-since we can have consistent importing syntax with babel,
-always use:
-
-```
-import some from 'some'
-```
-
--- hello.ts
+++ hello.ts
@@ -1,1 +1,35 @@
-console.log('Hello');
+type Text1 = {
+    $: {
+        type: 'Text1',
+    }
+    text: string;
+}
+
+type Number2 = {
+    $: {
+        type: 'Number2',
+    }
+    num: number;
+}
+
+type Field = Text1 | Number2;
+
+const field: Field = {
+    $: {
+        type: 'Text1',
+    },
+    text: 'hello',
+}
+
+function hello(field: Field) {
+    switch (field.$.type) {
+        case 'Text1':
+            console.log(field.text);
+            break;
+        case 'Number2':
+            console.log(field.num);
+            break;
+        default:
+            console.log('unknown');
+    }
+}
